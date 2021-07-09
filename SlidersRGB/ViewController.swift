f4104ce36536a4f39cdf64d267c4650ab661359e
immortalprogrammist <immortalvelial@icloud.com>
2021-07-09 16:04:25 +0200

    worked with ViewController.swift

--- SlidersRGB/ViewController.swift
+++ SlidersRGB/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var smallView: UIView!
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        smallView.layer.cornerRadius = 20
+        
+        redSlider.minimumTrackTintColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        greenSlider.minimumTrackTintColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        blueSlider.minimumTrackTintColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        getSliedersValue()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        getSliedersValue()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        getSliedersValue()
+    }
+    
+    private func getSliedersValue() {
+        smallView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
 }
 
