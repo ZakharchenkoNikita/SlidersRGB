ebf169c56b94a2aa465b43c0bc9322cba3359c82
immortalprogrammist <immortalvelial@icloud.com>
2021-07-23 18:58:50 +0200

    add delegate

--- SlidersRGB/MainViewController.swift
+++ SlidersRGB/MainViewController.swift
@@ -7,9 +7,37 @@
 
 import UIKit
 
+protocol SettingsViewControllerDelegate {
+    func setSmallViewColor(redSlider: CGFloat, greenSlider: CGFloat, blueSlider: CGFloat)
+}
+
 class MainViewController: UIViewController {
 
+    var redSliderValue: Float = 1.0
+    var greenSliderValue: Float = 1.0
+    var blueSliderValue: Float = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        
+        settingsVC.redSliderValue = redSliderValue
+        settingsVC.greenSliderValue = greenSliderValue
+        settingsVC.blueSliderValue = blueSliderValue
+        
+        settingsVC.delegate = self
+    }
+}
+
+extension MainViewController: SettingsViewControllerDelegate {
+    func setSmallViewColor(redSlider: CGFloat, greenSlider: CGFloat, blueSlider: CGFloat) {
+        view.backgroundColor = UIColor(red: redSlider, green: greenSlider, blue: blueSlider, alpha: 1)
+        
+        redSliderValue = Float(redSlider)
+        greenSliderValue = Float(greenSlider)
+        blueSliderValue = Float(blueSlider)
     }
 }
--- SlidersRGB/SettingsViewController.swift
+++ SlidersRGB/SettingsViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    // MARK: IB Outlets
     @IBOutlet weak var smallView: UIView!
     
     @IBOutlet weak var redValueLabel: UILabel!
@@ -19,35 +20,82 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
+    // MARK: properties
+    var smallViewColor: UIColor!
+    var delegate: SettingsViewControllerDelegate!
+    
+    var redSliderValue: Float!
+    var greenSliderValue: Float!
+    var blueSliderValue: Float!
+    
+    // MARK: override methods
     override func viewDidLoad() {
         super.viewDidLoad()
         smallView.layer.cornerRadius = 20
         
-        redSlider.minimumTrackTintColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        greenSlider.minimumTrackTintColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
-        blueSlider.minimumTrackTintColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        setupSliders()
+        setupLabel(for: redValueLabel, greenValueLabel, blueValueLabel)
+        setColor()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
-    @IBAction func redSliderAction() {
-        redValueLabel.text = String(format: "%.2f", redSlider.value)
-        getRGB()
+    // MARK: IB Actions
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider: setupLabel(for: redValueLabel)
+        case greenSlider: setupLabel(for: greenValueLabel)
+        default: setupLabel(for: blueValueLabel)
+        }
     }
     
-    @IBAction func greenSliderAction() {
-        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
-        getRGB()
+    @IBAction func doneButtonPressed() {
+        delegate.setSmallViewColor(redSlider: CGFloat(redSlider.value),
+                             greenSlider: CGFloat(greenSlider.value),
+                             blueSlider: CGFloat(blueSlider.value))
+        
+        navigationController?.popViewController(animated: true)
+        dismiss(animated: true, completion: nil)
     }
-    
-    @IBAction func blueSliderAction() {
-        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
-        getRGB()
-    }
-    
-    private func getRGB() {
+}
+
+// MARK: private methods
+extension SettingsViewController {
+    private func setColor() {
         smallView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
                                             green: CGFloat(greenSlider.value),
                                             blue: CGFloat(blueSlider.value),
                                             alpha: 1)
     }
+    
+    private func setupLabel(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redValueLabel:
+                label.text = getString(slider: redSlider)
+            case greenValueLabel:
+                label.text = getString(slider: greenSlider)
+            default:
+                label.text = getString(slider: blueSlider)
+            }
+        }
+    }
+    
+    private func getString(slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
+    private func setupSliders() {
+        redSlider.minimumTrackTintColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        greenSlider.minimumTrackTintColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        blueSlider.minimumTrackTintColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        
+        redSlider.value = redSliderValue
+        greenSlider.value = greenSliderValue
+        blueSlider.value = blueSliderValue
+    }
 }
-
